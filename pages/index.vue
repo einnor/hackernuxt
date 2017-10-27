16f7fe2541b5aae84036656d24e7b098a479dc37
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-10-27 16:09:52 +0300

    Create a Vuex Store for Global Data in Nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,11 @@
 </template>
 
 <script>
-  import axios from '~/plugins/axios'
+  import {mapState} from 'vuex'
 
   export default {
-    asyncData () {
-      return axios.get('users')
-        .then(res => ({
-          users: res.data
-        }))
-    }
+    computed: mapState([
+      'users'
+    ])
   }
 </script>
